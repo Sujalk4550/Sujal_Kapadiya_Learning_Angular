f10b1b131b3565559943adb84e20e1efe8e909e9
w0847992 <W0847992@myscc.ca>
2024-09-26 13:50:21 -0400

    Created 5 different properites

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgFor, NgIf} from "@angular/common";
+import {Motorcycle} from "./models/motorcycle";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Sujal4-Kapadiya-Learning-Angular';
-  name = 'Sujal Kapadiya';
-  age='19';
+  motorcyle1: Motorcycle ={brand:"Harley - Davidson",name:"Street Glide",colour:"Red",type:"Touring",petrol:true};
+  motorcyle2:  Motorcycle ={brand:"Yamaha",name:"YZF-R1",colour:"Blue",type:"Sportbike",petrol:true};
+  motorcyle3:  Motorcycle ={brand:"BMW",name:"R1250 GS",colour:"White",type:"Adventure",petrol:false};
+  motorcyle4 :Motorcycle ={brand:"Ducati",name:"Scrambler Icon",colour:"Yellow",type:"Scrambler",petrol:true};
+  motorcyle5: Motorcycle ={brand:"Honda",name:"Gold Wing",colour:"Back",type:"Touring",petrol:false};
+  motorcyle6 :Motorcycle ={brand:"Kawasaki",name:"Ninja ZX-10R",colour:"Green",type:"Sportsbike",petrol:true};
+
+  motocycleList:  Motorcycle[]=[this.motorcyle1,this.motorcyle2,this.motorcyle3,this.motorcyle4,this.motorcyle5,this.motorcyle6]
+
 }
