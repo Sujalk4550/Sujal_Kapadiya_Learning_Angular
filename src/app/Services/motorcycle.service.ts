87bb0b9e8f40e4ead98546de00d7d3f54b5fbeef
Sujal Kapadiya <W0847992myscc.ca>
2024-10-10 15:29:43 -0400

    Completed step 8 and 9

--- src/app/Services/motorcycle.service.ts
+++ src/app/Services/motorcycle.service.ts
@@ -7,10 +7,30 @@ import {motorcycleList} from "../Share/data/mock-motocycle";
   providedIn: 'root'
 })
 export class MotorcycleService {
-  private motorcycle : Motorcycle[] = motorcycleList;
+  private motorcycles : Motorcycle[] = motorcycleList;
 
   constructor() { }
   getMotorcycle():Observable<Motorcycle[]>{
     return of (motorcycleList);
+  }
+  getMotorcycleByBrand(brand: string): Observable<Motorcycle | undefined> {
+    const motorcycle = this.motorcycles.find(Motorcycle => brand == brand);
+    return  of (motorcycle);
+  }
+  addMotorcycle(newMotorcycle: Motorcycle): Observable <Motorcycle[]>{
+    this.motorcycles.push(newMotorcycle)
+    return of (this.motorcycles);
+  }
+  updateMotorcycle(updatedMotorcycle: Motorcycle): Observable<Motorcycle[]>{
+    const index = this.motorcycles.findIndex(Motorcycle => Motorcycle.brand !== updatedMotorcycle.brand);
+    if(index !== -1){
+      this.motorcycles[index]=updatedMotorcycle;
+    }
+    return of (this.motorcycles);
+  }
+
+  deleteMotorcycle(MotorcycleBrand : string) : Observable<Motorcycle[]>{
+    this.motorcycles = this.motorcycles.filter(Motorcycle => Motorcycle.brand !== MotorcycleBrand);
+    return of (this.motorcycles);
   }
 }
--- src/app/motorcyle-list/motorcyle-list.component.ts
+++ src/app/motorcyle-list/motorcyle-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {Motorcycle} from "../Share/models/motorcycle";
 import {MotorcycleListItemComponent} from "../motorcycle-list-item/motorcycle-list-item.component";
-import {NgClass, NgForOf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {MotorcycleService} from "../Services/motorcycle.service";
 
 @Component({
@@ -10,7 +10,8 @@ import {MotorcycleService} from "../Services/motorcycle.service";
   imports: [
     MotorcycleListItemComponent,
     NgClass,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './motorcyle-list.component.html',
   styleUrl: './motorcyle-list.component.css'
