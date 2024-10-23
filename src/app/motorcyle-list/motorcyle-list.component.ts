5470ea71f85b9d776e9b8702e75eb2cce2e45b8f
“Sujal <W0847992@myscc.ca>
2024-10-23 17:17:57 -0400

    Step 4,5 completed of assignment 4

--- src/app/motorcyle-list/motorcyle-list.component.ts
+++ src/app/motorcyle-list/motorcyle-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Motorcycle} from "../../Shared/models/motorcycle";
 import {MotorcycleListItemComponent} from "../motorcycle-list-item/motorcycle-list-item.component";
 import {NgClass, NgForOf} from "@angular/common";
+import {MotorcycleService} from "../Services/motorcycle.service";
 
 @Component({
   selector: 'app-motorcyle-list',
@@ -15,11 +16,9 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './motorcyle-list.component.css'
 })
 export class MotorcyleListComponent {
-  motorcyle1: Motorcycle ={brand:"Harley - Davidson",name:"Street Glide",colour:"Red",type:"Touring",petrol:true};
-  motorcyle2:  Motorcycle ={brand:"Yamaha",name:"YZF-R1",colour:"Blue",type:"Sportbike",petrol:true};
-  motorcyle3:  Motorcycle ={brand:"BMW",name:"R1250 GS",colour:"White",type:"Adventure",petrol:false};
-  motorcyle4 :Motorcycle ={brand:"Ducati",name:"Scrambler Icon",colour:"Yellow",type:"Scrambler",petrol:true};
+ motorcycleList: Motorcycle[]=[]
 
-  motorcycleList :  Motorcycle[]=[this.motorcyle1,this.motorcyle2,this.motorcyle3,this.motorcyle4]
+  constructor(private motorcycleService: MotorcycleService) {
+  }
 
 }
