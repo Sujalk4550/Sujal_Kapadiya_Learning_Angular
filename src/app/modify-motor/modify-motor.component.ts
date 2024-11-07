baddd8f32329f67e6a7375b5770663ec719dd02c
“Sujal <W0847992@myscc.ca>
2024-11-06 19:09:44 -0500

    Completed step 3,4 and 5 in assignment 6

--- src/app/modify-motor/modify-motor.component.ts
+++ src/app/modify-motor/modify-motor.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MotorcycleService} from "../Services/motorcycle.service";
+import {Motorcycle} from "../../Shared/models/motorcycle";
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-modify-motor',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './modify-motor.component.html',
   styleUrl: './modify-motor.component.css'
 })
-export class ModifyMotorComponent {
+export class ModifyMotorComponent implements OnInit{
 
+  motorcycleForm : FormGroup;
+  motorcycle : Motorcycle| undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    private route :ActivatedRoute,
+    private motorcycleService : MotorcycleService,
+    private router : Router
+  ) {
+    this.motorcycleForm = this.fb.group({
+      id: ['',Validators.required],
+      brand :['',Validators.required],
+      name :['',Validators.required],
+      colour :['',Validators.required],
+      type :['',Validators.required],
+      petrol : [false],
+    });
+  }
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.motorcycleService.getMotorcycleById(+id).subscribe(motorcycle => {
+        if (motorcycle) {
+          this.motorcycle = motorcycle;
+
+          this.motorcycleForm.patchValue(motorcycle);
+
+        }
+      });
+    }
+
+  }
+
+
+  onSubmit():void{
+    const motorcycle : Motorcycle = this.motorcycleForm.value;
+
+    if(motorcycle.id){
+      this.motorcycleService.updateMotorcycle(motorcycle);
+    }else {
+      const newId = this.motorcycleService.generateNewId();
+      motorcycle.id= newId;
+      this.motorcycleService.addMotorcycle(motorcycle);
+    }
+    this.router.navigate(['/motorcycle']);
+  }
+
+  navigateToMotorcycleList(): void{
+    this.router.navigate(['/motorcycle']);
+  }
 }
